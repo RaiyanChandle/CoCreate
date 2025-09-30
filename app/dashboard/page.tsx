25cc75ab560029a062bf81a6dc8f631df41f9c17
RaiyanChandle <rrchandle@gmail.com>
2025-09-30 19:06:44 +0530

    basic wireframe of dashboard ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,10 +4,7 @@ export default function Dashboard() {
   return (
     <>
       <SignedIn>
-        <div className="flex min-h-screen items-center justify-center">
-          <p className="text-2xl font-semibold">this is dashboard</p>
-          <UserButton/>
-        </div>
+        <div>dash board root page</div>
       </SignedIn>
       <SignedOut>
         <SignIn routing="hash"/>
